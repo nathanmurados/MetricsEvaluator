f99f6422e4116bd37ac007bebb4f15e1c39a3689
mbgh123 <mikeb.reg@blueyonder.co.uk>
2014-05-15 15:16:12 +0100

    Add JsVariableNameEvaluator and unit test

---
+++ MetricsEvaluationUtility/MetricsUtility.Tests/GetVariableNameTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MetricsUtiltiy.Tests
+{
+    using MetricsUtility.Core.Services.Evaluators.JavaScript;
+
+    [TestFixture]
+    public class GetVariableNameTests
+    {
+        [Test]
+        public void Extract_Varable_Name_1()
+        {
+            // Arrange
+            var evaluator = new JsVariableNameEvaluator();
+            string input = "@ViewData[\"Subject\"]";
+
+            // Act
+            string result = evaluator.Evaluate(input);
+
+            // Assert
+            Assert.AreEqual("ViewDataSubject", result);
+        }
+    }
+}
