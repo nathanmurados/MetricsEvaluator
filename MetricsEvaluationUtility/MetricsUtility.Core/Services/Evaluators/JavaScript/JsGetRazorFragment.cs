18e6eae6bac11f28737e47f5021e7bf7615b94f6
mbgh123 <mikeb.reg@blueyonder.co.uk>
2014-05-15 17:04:23 +0100

    add Get Razor fragment and unit tests
    add Get Razor fragment and unit tests

---
+++ MetricsEvaluationUtility/MetricsUtility.Core/Services/Evaluators/JavaScript/JsGetRazorFragment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MetricsUtility.Core.Services.Evaluators.JavaScript
+{
+    public class JsGetRazorFragment : IJsGetRazorFragment
+    {
+        /// <summary>
+        /// Extract the razor code from the line of javascript
+        /// Input: A line of Javascript containing an @. The @ prefixes razor code
+        /// Note the line may contain several fragments of razor, but only the first (working left ro right) should be processed
+        /// The razor code is prefixed with @, it will probably be surrounded by quotes (single or double).
+        /// </summary>
+        public string GetFragment(string jsLine)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MetricsEvaluationUtility/MetricsUtility.Tests/GetRazorCodeTests.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+
+namespace MetricsUtiltiy.Tests
+{
+    using MetricsUtility.Core.Services.Evaluators.JavaScript;
+
+    [TestFixture]
+    public class GetRazorCodeTests
+    {
+        [Test]
+        public void Extract_Razor_1()
+        {
+            // Arrange
+            var evaluator = new JsGetRazorFragment();
+            string input = "var selectedMenu = '@ViewBag.MenuInstanceName';";
+
+            // Act
+            string result = evaluator.GetFragment(input);
+
+            // Assert
+            Assert.AreEqual("ViewBag.MenuInstanceName", result);
+        }
+
+        [Test]
+        public void Extract_Razor_2()
+        {
+            // Arrange
+            var evaluator = new JsGetRazorFragment();
+            string input = "var addPageUrl = '@Url.Action(\"Configure\", \"ConfigureMenu\")';";
+
+            // Act
+            string result = evaluator.GetFragment(input);
+
+            // Assert
+            Assert.AreEqual("Url.Action(\"Configure\", \"ConfigureMenu\")", result);
+        }
+        [Test]
+        public void Extract_Razor_3()
+        {
+            // Arrange
+            var evaluator = new JsGetRazorFragment();
+            string input = "$('#DecommisionReason').val('@decommisionReason');";
+
+            // Act
+            string result = evaluator.GetFragment(input);
+
+            // Assert
+            Assert.AreEqual("@decommisionReason", result);
+        }
+        [Test]
+        public void Extract_Razor_4()
+        {
+            // Arrange
+            var evaluator = new JsGetRazorFragment();
+            string input = "globalFunction = @Html.Raw(Newtonsoft.Json.JsonConvert.SerializeObject(Model.GlobalFunctionVmList));";
+
+            // Act
+            string result = evaluator.GetFragment(input);
+
+            // Assert
+            Assert.AreEqual("Html.Raw(Newtonsoft.Json.JsonConvert.SerializeObject(Model.GlobalFunctionVmList))", result);
+        }
+        [Test]
+        public void Extract_Razor_5()
+        {
+            // Arrange
+            var evaluator = new JsGetRazorFragment();
+            
+            // NB contains 2 fragments of razor
+            string input = " data: \"{'docId1':'\" + '@ViewBag.docid' + \"','conditionType1':'\" + '@ViewBag.doctype' + \"'}\",";
+
+            // Act
+            string result = evaluator.GetFragment(input);
+
+            // Assert
+            Assert.AreEqual("@ViewBag.docid", result);
+        }
+        [Test]
+        public void Extract_Razor_6()
+        {
+            // Arrange
+            var evaluator = new JsGetRazorFragment();
+            string input = "$('#HiddenName').val('@Convert.ToString(stateWatcherVM.LName)');";
+
+            // Act
+            string result = evaluator.GetFragment(input);
+
+            // Assert
+            Assert.AreEqual("@Convert.ToString(stateWatcherVM.LName)", result);
+        }
+        
+    }
+}
