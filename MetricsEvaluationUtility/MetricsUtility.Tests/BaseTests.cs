cbe7fc938c025c97cfaa9ef98464649903c363bb
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2014-05-15 18:42:02 +0100

    Finally, committing base tests.

--- MetricsEvaluationUtility/MetricsUtility.Tests/BaseTests.cs
+++ MetricsEvaluationUtility/MetricsUtility.Tests/BaseTests.cs
@@ -2,16 +2,24 @@ using System.IO;
 
 namespace MetricsUtiltiy.Tests
 {
+    using System.Reflection;
+
     public abstract class BaseTests
     {
         public FileAndContents GetFileAndContent()
         {
-            const string path = @"C:\Users\Michael.Brown\Documents\GitHub\MetricsEvaluator\MetricsEvaluationUtility\MetricsUtility.Tests\Assets\TestingResource.cshtml";
-
-            return new FileAndContents
+            const string path = "MetricsUtiltiy.Tests.Assets.TestingResource.cshtml";
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string fileText = null;
+            using (Stream s = asm.GetManifestResourceStream("MetricsUtiltiy.Tests.Assets.TestingResource.cshtml"))
+            using (TextReader r = new StreamReader(s))
             {
-                Path = path,
-                Contents = File.ReadAllLines(path)
+                fileText = r.ReadToEnd();
+            }
+                return new FileAndContents
+            {
+                Path =path,
+                Contents =fileText.Split(new char[] {'\n'})
             };
         }
     }
