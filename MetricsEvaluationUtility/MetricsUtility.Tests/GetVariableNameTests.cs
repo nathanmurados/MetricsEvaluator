7afc432b393d7655cfab2210e98c621c5dd87079
mbgh123 <mikeb.reg@blueyonder.co.uk>
2014-05-15 15:49:07 +0100

    more get variable tests
    more get variable tests

--- MetricsEvaluationUtility/MetricsUtility.Tests/GetVariableNameTests.cs
+++ MetricsEvaluationUtility/MetricsUtility.Tests/GetVariableNameTests.cs
@@ -25,5 +25,131 @@ namespace MetricsUtiltiy.Tests
             // Assert
             Assert.AreEqual("ViewDataSubject", result);
         }
+
+        [Test]
+        public void Extract_Varable_Name_2()
+        {
+            // Arrange
+            var evaluator = new JsVariableNameEvaluator();
+            string input = "@Model[0].MessageList[ActivitiesConstants.PleaseEnterValue]";
+
+            // Act
+            string result = evaluator.Evaluate(input);
+
+            // Assert
+            Assert.AreEqual("Model0MessageListActivitiesConstantsPleaseEnterValue", result);
+        }
+
+        [Test]
+        public void Extract_Varable_Name_3()
+        {
+            // Arrange
+            var evaluator = new JsVariableNameEvaluator();
+            string input = "@ViewBag.pageId";
+
+            // Act
+            string result = evaluator.Evaluate(input);
+
+            // Assert
+            Assert.AreEqual("ViewBagpageId", result);
+        }
+
+        [Test]
+        public void Extract_Varable_Name_4()
+        {
+            // Arrange
+            var evaluator = new JsVariableNameEvaluator();
+            string input = "@Model.SettingsFacts[ActivitiesConstants.PhoneCallNotes]";
+
+            // Act
+            string result = evaluator.Evaluate(input);
+
+            // Assert
+            Assert.AreEqual("ModelSettingsFactsActivitiesConstantsPhoneCallNotes]", result);
+        }
+
+        [Test]
+        public void Extract_Varable_Name_5()
+        {
+            // Arrange
+            var evaluator = new JsVariableNameEvaluator();
+            string input = "@errorMessage";
+
+            // Act
+            string result = evaluator.Evaluate(input);
+
+            // Assert
+            Assert.AreEqual("errorMessage", result);
+        }
+
+        [Test]
+        public void Extract_Varable_Name_6()
+        {
+            // Arrange
+            var evaluator = new JsVariableNameEvaluator();
+            string input = "@Html.Raw(ViewBag.factPageDict)";
+
+            // Act
+            string result = evaluator.Evaluate(input);
+
+            // Assert
+            Assert.AreEqual("HtmlRawViewBag.factPageDict", result);
+        }
+
+        [Test]
+        public void Extract_Varable_Name_7()
+        {
+            // Arrange
+            var evaluator = new JsVariableNameEvaluator();
+            string input = "@Model.MessageList[\"MSG1005884\"]";
+
+            // Act
+            string result = evaluator.Evaluate(input);
+
+            // Assert
+            Assert.AreEqual("ModelMessageListMSG1005884", result);
+        }
+
+        [Test]
+        public void Extract_Varable_Name_8()
+        {
+            // Arrange
+            var evaluator = new JsVariableNameEvaluator();
+            string input = "@Url.Action(\"MenuAssociation\", \"ConfigureMenu\")";
+
+            // Act
+            string result = evaluator.Evaluate(input);
+
+            // Assert
+            Assert.AreEqual("UrlActionMenuAssociationConfigureMenu", result);
+        }
+
+        [Test]
+        public void Extract_Varable_Name_9()
+        {
+            // Arrange
+            var evaluator = new JsVariableNameEvaluator();
+            string input = "@Convert.ToInt32(ViewBag.PageNo);";
+
+            // Act
+            string result = evaluator.Evaluate(input);
+
+            // Assert
+            Assert.AreEqual("ConvertToInt32ViewBagPageNo", result);
+        }
+
+        [Test]
+        public void Extract_Varable_Name_10()
+        {
+            // Arrange
+            var evaluator = new JsVariableNameEvaluator();
+            string input = "@(new HtmlString(Json.Encode(Model.Frequency)));";
+
+            // Act
+            string result = evaluator.Evaluate(input);
+
+            // Assert
+            Assert.AreEqual("newHtmlStringJsonEncodeModelFrequency", result);
+        }
     }
 }
