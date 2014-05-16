be910417b68c163a0bb2cab5d78ea645087cd7c1
Nathan Murados <nathan@hardcoresoftware.co.uk>
2014-05-16 16:22:29 +0100

    test

---
+++ MetricsEvaluationUtility/MetricsUtility.Core/Services/Evaluators/JavaScript/JsModuleBlockEvaluator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using MetricsUtility.Core.ViewModels;
+
+namespace MetricsUtility.Core.Services.Evaluators.JavaScript
+{
+    public class JsModuleBlockEvaluator : IJsModuleBlockEvaluator
+    {
+        public IJsModuleLineEvaluator JsModuleLineEvaluator { get; private set; }
+        public JsModuleBlockEvaluator(IJsModuleLineEvaluator jsModuleLineEvaluator)
+        {
+            JsModuleLineEvaluator = jsModuleLineEvaluator;
+        }
+
+
+        public IEnumerable<JsModuleViewModel> Evaluate(IEnumerable<string> jsLines)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
