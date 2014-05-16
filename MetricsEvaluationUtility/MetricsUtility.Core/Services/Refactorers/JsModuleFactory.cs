dcd253643fa1fe451ed246a6a35dd1652daed3c8
Nathan Murados <nathan@hardcoresoftware.co.uk>
2014-05-16 11:31:43 +0100

    x

--- MetricsEvaluationUtility/MetricsUtility.Core/Services/Refactorers/JsModuleFactory.cs
+++ MetricsEvaluationUtility/MetricsUtility.Core/Services/Refactorers/JsModuleFactory.cs
@@ -1,10 +1,11 @@
-using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using MetricsUtility.Core.ViewModels;
 
 namespace MetricsUtility.Core.Services.Refactorers
 {
     public class JsModuleFactory : IJsModuleFactory
     {
-        public static object Build()
+        public static string[] Build(List<JsModuleViewModel> data)
         {
             throw new System.NotImplementedException();
         }
--- MetricsEvaluationUtility/MetricsUtility.Tests/JsModuleFactoryTests.cs
+++ MetricsEvaluationUtility/MetricsUtility.Tests/JsModuleFactoryTests.cs
@@ -14,12 +14,22 @@ namespace MetricsUtiltiy.Tests
 
             var data = new List<JsModuleViewModel>
             {
-                new JsModuleViewModel{JavaScriptName = "ap2.ViewDataSubject",OriginalRazorText = "@ViewData[\"Subject\"]"},
-                new JsModuleViewModel{JavaScriptName = "ap2.Model0MessageListActivitiesConstantsPleaseEnterValue",OriginalRazorText = "@Model[0].MessageList[ActivitiesConstants.PleaseEnterValue]"},
-                new JsModuleViewModel{JavaScriptName = "ap2.ViewBag.pageId",OriginalRazorText = "@ViewData[\"Subject\"]"},
+                new JsModuleViewModel{ JavaScriptName = "ap2.ViewDataSubject", OriginalRazorText = "'@ViewData[\"Subject\"]'" },
+                new JsModuleViewModel{ JavaScriptName = "ap2.Model0MessageListActivitiesConstantsPleaseEnterValue", OriginalRazorText = "'@Model[0].MessageList[ActivitiesConstants.PleaseEnterValue]'" },
+                new JsModuleViewModel{ JavaScriptName = "ap2.ModelMessageListMSG1005884", OriginalRazorText = "'@Model.MessageList[\"MSG1005884\"]'"},
+                new JsModuleViewModel{ JavaScriptName = "ap2.SomeInt", OriginalRazorText = "@Model.SomeInt"},
             };
 
-            var result = JsModuleFactory.Build();
+            var result = JsModuleFactory.Build(data);
+
+            Assert.AreEqual("var ap2 = (function(ap2) {", result[0]);
+            Assert.AreEqual("   ap2.ViewDataSubject = '@ViewData[\"Subject\"]';", result[0]);
+            Assert.AreEqual("   ap2.Model0MessageListActivitiesConstantsPleaseEnterValue = '@Model[0].MessageList[ActivitiesConstants.PleaseEnterValue]';", result[0]);
+            Assert.AreEqual("   ap2.ModelMessageListMSG1005884 = '@Model.MessageList[\"MSG1005884\"]';", result[0]);
+            Assert.AreEqual("   ap2.SomeInt = @Model.SomeInt;", result[0]);
+            Assert.AreEqual("   return ap2;", result[0]);
+            Assert.AreEqual("} (ap2 || {}));", result[0]);
+
         }
     }
 
