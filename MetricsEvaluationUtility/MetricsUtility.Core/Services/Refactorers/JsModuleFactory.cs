0903cece644db71b51f797754331637beec2ff9b
mbgh123 <mikeb.reg@blueyonder.co.uk>
2014-05-16 12:37:27 +0100

    JsModuleFactory - first cut
    JsModuleFactory - first cut

--- MetricsEvaluationUtility/MetricsUtility.Core/Services/Refactorers/JsModuleFactory.cs
+++ MetricsEvaluationUtility/MetricsUtility.Core/Services/Refactorers/JsModuleFactory.cs
@@ -3,11 +3,52 @@ using MetricsUtility.Core.ViewModels;
 
 namespace MetricsUtility.Core.Services.Refactorers
 {
+    using System;
+
     public class JsModuleFactory : IJsModuleFactory
     {
+        private const string jsContainerName = "ap2";
+
+        /// <summary>
+        /// Takes a list of razor fragments and corresponding variables names and turns into a JS module.
+        /// </summary>
         public static string[] Build(List<JsModuleViewModel> data)
         {
-            throw new System.NotImplementedException();
+            // Currently the razor fragment is surrounded by quotes, but that may not always be the case...
+            //
+            // input:
+            //      supplierList    '@supplierList'
+            //      elements        '@elements'
+            //
+            // output:
+            //    var ap2 = (function (ap2) {
+            //        ap2.supplierList = '@supplierList';
+            //        ap2.elements = '@elements';
+            //        return ap2;
+            //    }(ap2 || {}));
+
+            if (data == null || data.Count == 0)
+            {
+                throw new ArgumentException("Null or empty arguments.");
+            }
+
+            int i = 0;
+            int moduleLines = data.Count + 3;
+            string indent1 = "    ";       // may change to tabs
+            string indent2 = "        ";   // may change to tabs
+            string[] result = new string[moduleLines];
+
+            result[i++] = String.Format("{0}var {1} = (function({1}) {{",indent1, jsContainerName);
+
+            foreach (JsModuleViewModel item in data)
+            {
+                result[i++] = string.Format("{0}{1}.{2} = {3};", indent2, jsContainerName ,item.JavaScriptName, item.OriginalRazorText);
+            }
+
+            result[i++] = String.Format("{0}return {1};", indent2, jsContainerName);
+            result[i++] = String.Format("{0}}} ({1} || {{}}));",indent1, jsContainerName);
+
+            return result;
         }
     }
 }
--- MetricsEvaluationUtility/MetricsUtility.Tests/JsModuleFactoryTests.cs
+++ MetricsEvaluationUtility/MetricsUtility.Tests/JsModuleFactoryTests.cs
@@ -8,27 +8,28 @@ namespace MetricsUtiltiy.Tests
     [TestFixture]
     public class JsModuleFactoryTests
     {
+        [Test]
         public void Test1()
         {
             var obj = new JsModuleFactory();
 
             var data = new List<JsModuleViewModel>
             {
-                new JsModuleViewModel{ JavaScriptName = "ap2.ViewDataSubject", OriginalRazorText = "'@ViewData[\"Subject\"]'" },
-                new JsModuleViewModel{ JavaScriptName = "ap2.Model0MessageListActivitiesConstantsPleaseEnterValue", OriginalRazorText = "'@Model[0].MessageList[ActivitiesConstants.PleaseEnterValue]'" },
-                new JsModuleViewModel{ JavaScriptName = "ap2.ModelMessageListMSG1005884", OriginalRazorText = "'@Model.MessageList[\"MSG1005884\"]'"},
-                new JsModuleViewModel{ JavaScriptName = "ap2.SomeInt", OriginalRazorText = "@Model.SomeInt"},
+                new JsModuleViewModel{ JavaScriptName = "ViewDataSubject", OriginalRazorText = "'@ViewData[\"Subject\"]'" },
+                new JsModuleViewModel{ JavaScriptName = "Model0MessageListActivitiesConstantsPleaseEnterValue", OriginalRazorText = "'@Model[0].MessageList[ActivitiesConstants.PleaseEnterValue]'" },
+                new JsModuleViewModel{ JavaScriptName = "ModelMessageListMSG1005884", OriginalRazorText = "'@Model.MessageList[\"MSG1005884\"]'"},
+                new JsModuleViewModel{ JavaScriptName = "SomeInt", OriginalRazorText = "@Model.SomeInt"},
             };
 
             var result = JsModuleFactory.Build(data);
 
-            Assert.AreEqual("var ap2 = (function(ap2) {", result[0]);
-            Assert.AreEqual("   ap2.ViewDataSubject = '@ViewData[\"Subject\"]';", result[0]);
-            Assert.AreEqual("   ap2.Model0MessageListActivitiesConstantsPleaseEnterValue = '@Model[0].MessageList[ActivitiesConstants.PleaseEnterValue]';", result[0]);
-            Assert.AreEqual("   ap2.ModelMessageListMSG1005884 = '@Model.MessageList[\"MSG1005884\"]';", result[0]);
-            Assert.AreEqual("   ap2.SomeInt = @Model.SomeInt;", result[0]);
-            Assert.AreEqual("   return ap2;", result[0]);
-            Assert.AreEqual("} (ap2 || {}));", result[0]);
+            Assert.AreEqual("    var ap2 = (function(ap2) {", result[0]);
+            Assert.AreEqual("        ap2.ViewDataSubject = '@ViewData[\"Subject\"]';", result[1]);
+            Assert.AreEqual("        ap2.Model0MessageListActivitiesConstantsPleaseEnterValue = '@Model[0].MessageList[ActivitiesConstants.PleaseEnterValue]';", result[2]);
+            Assert.AreEqual("        ap2.ModelMessageListMSG1005884 = '@Model.MessageList[\"MSG1005884\"]';", result[3]);
+            Assert.AreEqual("        ap2.SomeInt = @Model.SomeInt;", result[4]);
+            Assert.AreEqual("        return ap2;", result[5]);
+            Assert.AreEqual("    } (ap2 || {}));", result[6]);
 
         }
     }
